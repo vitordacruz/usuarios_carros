7fb3980f4f1b789bec5d96643de095aec97de70f
Vitor <vitordacruz@gmail.com>
2023-12-12 17:53:08 -0300

    Criação do projeto

---
+++ src/main/java/br/com/examplo/sistema/usuariocarros/UsuarioCarrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.examplo.sistema.usuariocarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioCarrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuarioCarrosApplication.class, args);
+    }
+
+}

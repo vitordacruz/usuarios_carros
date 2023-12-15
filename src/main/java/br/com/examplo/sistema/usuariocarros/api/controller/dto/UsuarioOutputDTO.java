dc724d90303755682e263b7d8320dc96795fbda9
Vitor <vitordacruz@gmail.com>
2023-12-15 18:54:23 -0300

    2. Cadastro Carros - Ajuste na busca de usuarios

--- src/main/java/br/com/examplo/sistema/usuariocarros/api/controller/dto/UsuarioOutputDTO.java
+++ src/main/java/br/com/examplo/sistema/usuariocarros/api/controller/dto/UsuarioOutputDTO.java
@@ -3,6 +3,7 @@ package br.com.examplo.sistema.usuariocarros.api.controller.dto;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +15,7 @@ public class UsuarioOutputDTO {
     private String firstName;
     private String lastName;
     private String email;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate birthday;
     private String login;
     private String phone;
